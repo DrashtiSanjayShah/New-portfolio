9e1a6500b81c7b94a5270c716113507c1b26528b
DrashtiS <94853646+DrashtiSanjayShah@users.noreply.github.com>
2026-09-01 22:39:47 +0530

    Fix capitalization in FilterBar categories

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -6,13 +6,13 @@ import Resume from '../assets/Resume.pdf';
 const categories = [
   { name: 'Resume', external: Resume, download: true },
   { name: 'Must watch YT videos', external: 'https://youtube.com/playlist?list=PLi2uDlkkRasLwv4rDnaLF8CB9lCD-Qp7e&si=Jh-rh_iBVVIN3frC' },
-  { name: 'Freelance Work', scrollTo: 'my-projects' },
-  { name: 'Placement Tips & Tricks', external: 'https://www.youtube.com/playlist?list=PLI9IU_4HLCWGticImP63VgDx3XG83LsOp' },
+  { name: 'Freelance work', scrollTo: 'my-projects' },
+  { name: 'Placement tips & tricks', external: 'https://www.youtube.com/playlist?list=PLI9IU_4HLCWGticImP63VgDx3XG83LsOp' },
   { name: 'Linkedin', external: 'https://www.linkedin.com/in/drashtisanjayshah/' },
   { name: 'GitHub', external: 'https://github.com/DrashtiSanjayShah' },
   { name: 'Certifications', external: 'https://drive.google.com/drive/folders/16LAVwVvn2WORqqTWsg9Hzt7KAdHXK0xS?usp=share_link' },
-  { name: 'My Fav YT channels', path: '/channels' },
-  { name: 'Project Recommendations', path: '/projects' },
+  { name: 'My favourite YT channels', path: '/channels' },
+  { name: 'Project recommendations', path: '/projects' },
 ];
 
 const FilterBar = () => {
